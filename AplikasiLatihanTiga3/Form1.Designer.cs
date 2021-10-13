7282866e985fe53f19c9d5c556a20269aaa3d275
ASUS <ASUS@LAPTOP-CVSVBJP3>
2021-10-13 22:49:46 +0700

    Add project files.

---
+++ AplikasiLatihanTiga3/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace AplikasiLatihanTiga3
+{
+    partial class frmLatihanTiga3
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.cmbPendidikan = new System.Windows.Forms.ComboBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.cmbPekerjaan = new System.Windows.Forms.ComboBox();
+            this.lstPesan1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pendidikan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pekerjaan";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtNama
+            // 
+            this.txtNama.BackColor = System.Drawing.Color.Lavender;
+            this.txtNama.Location = new System.Drawing.Point(116, 25);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(336, 31);
+            this.txtNama.TabIndex = 4;
+            this.txtNama.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cmbPendidikan
+            // 
+            this.cmbPendidikan.BackColor = System.Drawing.Color.Lavender;
+            this.cmbPendidikan.FormattingEnabled = true;
+            this.cmbPendidikan.Items.AddRange(new object[] {
+            "SD",
+            "SMP",
+            "SMA",
+            "Diploma",
+            "Sarjana",
+            "PascaSarjana"});
+            this.cmbPendidikan.Location = new System.Drawing.Point(116, 62);
+            this.cmbPendidikan.Name = "cmbPendidikan";
+            this.cmbPendidikan.Size = new System.Drawing.Size(336, 33);
+            this.cmbPendidikan.TabIndex = 7;
+            // 
+            // btnOk
+            // 
+            this.btnOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnOk.Location = new System.Drawing.Point(31, 384);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(421, 39);
+            this.btnOk.TabIndex = 9;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = false;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // cmbPekerjaan
+            // 
+            this.cmbPekerjaan.BackColor = System.Drawing.Color.Lavender;
+            this.cmbPekerjaan.FormattingEnabled = true;
+            this.cmbPekerjaan.Items.AddRange(new object[] {
+            "Pedangang",
+            "PNS",
+            "Dosen",
+            "Swasta",
+            "ABRI",
+            "Lain-Lain"});
+            this.cmbPekerjaan.Location = new System.Drawing.Point(116, 101);
+            this.cmbPekerjaan.Name = "cmbPekerjaan";
+            this.cmbPekerjaan.Size = new System.Drawing.Size(336, 33);
+            this.cmbPekerjaan.TabIndex = 10;
+            // 
+            // lstPesan1
+            // 
+            this.lstPesan1.BackColor = System.Drawing.Color.Lavender;
+            this.lstPesan1.Enabled = false;
+            this.lstPesan1.FormattingEnabled = true;
+            this.lstPesan1.ItemHeight = 25;
+            this.lstPesan1.Location = new System.Drawing.Point(31, 157);
+            this.lstPesan1.Name = "lstPesan1";
+            this.lstPesan1.Size = new System.Drawing.Size(420, 204);
+            this.lstPesan1.TabIndex = 11;
+            // 
+            // frmLatihanTiga3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(477, 444);
+            this.Controls.Add(this.lstPesan1);
+            this.Controls.Add(this.cmbPekerjaan);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.cmbPendidikan);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLatihanTiga3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Latihan 3.3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.ComboBox cmbPendidikan;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.ComboBox cmbPekerjaan;
+        private System.Windows.Forms.ListBox lstPesan1;
+    }
+}
+
---
+++ AplikasiLatihanTiga3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiLatihanTiga3
+{
+    public partial class frmLatihanTiga3 : Form
+    {
+        public frmLatihanTiga3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            var nama = txtNama.Text;
+            var pendidikan = cmbPendidikan.Text;
+            var pekerjaan = cmbPekerjaan.Text;
+
+            lstPesan1.Items.Add(string.Format("Halo {0}", nama));
+            lstPesan1.Items.Add(string.Format("Pendidikan Anda {0} ya?", pendidikan));
+            lstPesan1.Items.Add(string.Format("Anda Seorang {0} , Hebat !!!", pekerjaan));
+
+        }
+    }
+}
